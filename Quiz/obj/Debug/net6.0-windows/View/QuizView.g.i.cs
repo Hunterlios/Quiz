53df7ac22edf3ba7aef0028d29dd5a3d2105bdc9
Hunterlios <jakubx1001@gmail.com>
2023-05-26 16:20:03 +0200

    feat: minor changes ✏️

--- Quiz/obj/Debug/net6.0-windows/View/QuizView.g.i.cs
+++ Quiz/obj/Debug/net6.0-windows/View/QuizView.g.i.cs
@@ -62,7 +62,7 @@ namespace Quiz.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Quiz;component/view/quizview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Quiz;V1.0.0.0;component/view/quizview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\QuizView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
